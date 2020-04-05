8c149964386521ea7e0ae9a65cff059dc1c94d73
Rodrigue <rodriguealcazar@gmail.com>
2020-04-05 16:35:49 +0200

    Add unit test for full program.

--- 2020/qualification/parenting/test_solution.py
+++ 2020/qualification/parenting/test_solution.py
@@ -1,6 +1,9 @@
 import unittest
+from contextlib import redirect_stdout
+from io import StringIO
+from unittest.mock import patch
 
-from solution import assign_activities
+from solution import assign_activities, main
 
 
 class TestAssignActivities(unittest.TestCase):
@@ -48,6 +51,45 @@ class TestAssignActivities(unittest.TestCase):
             ['C', 'C'],
             assign_activities(test_activities)
         )
+
+
+class TestFullInput(unittest.TestCase):
+
+    def test_sample_input(self):
+        sample_input = (
+            "4\n",
+            "3\n",
+            "360 480\n",
+            "420 540\n",
+            "600 660\n",
+            "3\n",
+            "0 1440\n",
+            "1 3\n",
+            "2 4\n",
+            "5\n",
+            "99 150\n",
+            "1 100\n",
+            "100 301\n",
+            "2 5\n",
+            "150 250\n",
+            "2\n",
+            "0 720\n",
+            "720 1440\n"
+        )
+        expected_output = (
+            "Case #1: CJC\n" +
+            "Case #2: IMPOSSIBLE\n" +
+            "Case #3: JCCJJ\n" +
+            "Case #4: CC\n"
+        )
+
+        mock_stdout = StringIO()
+        with patch('builtins.input', side_effect=sample_input):
+            with redirect_stdout(mock_stdout):
+                main()
+
+        self.assertEqual(expected_output, mock_stdout.getvalue())
+
 
 if __name__ == '__main__':
     unittest.main()
